cb29725f73cf9c42e2e990ed8f4ba234ca583b12
Georg Dronov <dronovgeorg72@gmail.com>
2024-06-28 17:05:54 +0300

    finish long pulling

--- client/real-time-course/README.md
+++
@@ -1,3 +0,0 @@
-# Учебный проект по работе с сервером и клиентом
-
-Тут рассмотрю Long polling, event sourcing, websockets & комнаты
--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import LongPulling from './LongPulling';
 
 function App() {
   return (
     <div className="App">
-     
+      <LongPulling/>
     </div>
   );
 }
